548b742dedfe2ccaacc8a73dfb394ae5b07cd7f3
Arshia-r0 <arshia009k@proton.me>
2024-09-14 20:15:39 +0330

    dataStore fix

--- app/src/main/java/com/arshia/musicplayer/MainActivity.kt
+++ app/src/main/java/com/arshia/musicplayer/MainActivity.kt
@@ -4,10 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.getValue
-import androidx.hilt.navigation.compose.hiltViewModel
+import com.arshia.musicplayer.data.Preferences
 import com.arshia.musicplayer.presentation.permissions.Permissions
-import com.arshia.musicplayer.presentation.settings.SettingsViewModel
 import com.arshia.musicplayer.presentation.theme.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,11 +16,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        Preferences.getPreferences()
         setContent {
-            val settingsViewModel: SettingsViewModel = hiltViewModel()
-            val theme by settingsViewModel.settings
-            AppTheme(theme = theme.appTheme) {
-                Permissions(this)
+            AppTheme(theme = Preferences.state.value.appTheme) {
+                Permissions(this@MainActivity)
             }
         }
     }
--- app/src/main/java/com/arshia/musicplayer/presentation/settings/SettingsViewModel.kt
+++ app/src/main/java/com/arshia/musicplayer/presentation/settings/SettingsViewModel.kt
@@ -1,56 +1,16 @@
 package com.arshia.musicplayer.presentation.settings
 
-import android.content.Context
-import android.os.Build
-import androidx.compose.runtime.mutableStateOf
-import androidx.datastore.dataStore
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.arshia.musicplayer.common.next
-import com.arshia.musicplayer.data.model.settings.Settings
+import com.arshia.musicplayer.data.Preferences
 import com.arshia.musicplayer.data.model.settings.Themes
-import com.arshia.musicplayer.data.repository.serializers.SettingsSerializer
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.android.qualifiers.ApplicationContext
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
-
-private val Context.dataStore by dataStore(
-    "settings.json",
-    SettingsSerializer
-)
 
 @HiltViewModel
-class SettingsViewModel @Inject constructor(
-    @ApplicationContext context: Context,
-): ViewModel() {
+class SettingsViewModel: ViewModel() {
 
-    private val dataStore = context.dataStore
-    val settings = mutableStateOf(Settings())
+    val settings = Preferences.state
 
-    init {
-        getPreferences()
-    }
-
-    fun changeTheme(currentTheme: Themes) {
-        viewModelScope.launch(Dispatchers.IO) {
-            val newTheme = currentTheme.next()
-            if(Build.VERSION.SDK_INT < 31 && newTheme == Themes.DynamicColor)
-                newTheme.next()
-            dataStore.updateData {
-                it.copy(appTheme = newTheme)
-            }
-        }
-    }
-
-    private fun getPreferences() {
-        dataStore.data.onEach { result ->
-            settings.value = settings.value.copy(appTheme = result.appTheme)
-        }.launchIn(viewModelScope)
-    }
+    fun changeTheme(theme: Themes) = Preferences.changeTheme(theme)
 
 }
